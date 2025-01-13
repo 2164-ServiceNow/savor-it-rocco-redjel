f47fe4763b0c0f27b16eb684708185f034483dee
NickRoccodev11 <nchls.rcc@gmail.com>
2025-01-13 14:34:31 -0500

    Establish view templates, add angular-routes dependency, and format homepage. Navigator: Abdelouahed Driver: Nick

--- controllers/app.js
+++ controllers/app.js
@@ -1,4 +1,33 @@
-let savorIt = angular.module("savorIt", []);
+let savorIt = angular.module("savorIt", ["ngRoute"]);
+
+savorIt.config([
+  "$routeProvider", '$locationProvider',
+  function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix("");
+    $routeProvider
+      // .when("/", {
+      //   templateUrl: "../views/index.html",
+      //   controller: "HomeController",
+      // })
+      .when("/search", {
+        templateUrl: "../views/search.html",
+        controller: "SearchController",
+      })
+      .when("/categories",{
+        templateUrl: "../views/categories.html",
+        controller: "CategoriesController"
+      })
+      .when("/random",{
+        templateUrl: "../views/random.html",
+         controller: "RandomController"
+      })
+      .otherwise({
+        redirectTo: "/",
+      });
+  },
+]);
+
+
 
 savorIt.controller("HomeController", [
   "$scope",
@@ -6,3 +35,14 @@ savorIt.controller("HomeController", [
     $scope.message = "hello from the controller";
   },
 ]);
+
+savorIt.controller("SearchController",["$scope", function($scope){
+
+}])
+savorIt.controller("CategoriesController",["$scope", function($scope){
+
+}])
+savorIt.controller("RandomController",["$scope", function($scope){
+
+}])
+
