88ecb1a314d4bfde1f06b47b68766f5be94ed961
Abdelouahed Redjel <abdelouahedredjel@gmail.com>
2025-01-15 13:46:10 -0600

    Add some functionalities on the search by name and by areas user stories, Driver: Abdelouahed, Observer: Nicholas

--- components/search/search.controller.js
+++ components/search/search.controller.js
@@ -1,24 +1,57 @@
-angular.module("search", []).controller("SearchController", [
+angular
+.module("search", [])
+.controller(
+  "SearchController", [
   "$scope",
   "SearchService",
   function ($scope, SearchService) {
-    $scope.message = "hi from searchcontroller";
-    $scope.searchValue = "";
-    // $scope.foods = "";
+    $scope.searchValue = '';
+    $scope.selectedArea = '';
+    $scope.errorMessage = '';
+    $scope.areas = [];
+    $scope.foods = [];
+
+    $scope.init = function(){
+      console.log("init function called")
+      const areasPromise = SearchService.getAreas();
+      areasPromise
+        .then(function(response){
+          if(response.data.meals){
+            $scope.areas = response.data.meals.map(function(meal){
+              return meal.strArea;
+            })
+          }
+        })
+        .catch(function(response){
+            $scope.errorMessage = "Failed to load areas."
+        });
+    };
+
     $scope.search = function () {
-      const searchPromise = SearchService.searchByName($scope.searchValue);
+      $scope.foods = [];
+      $scope.errorMessage = '';
+
+      if(!$scope.selectedArea && !$scope.searchValue){
+        $scope.errorMessage = 'Please provide a name or select an area to search';
+        return;
+      }
+
+      const searchPromise = $scope.searchValue ? 
+         SearchService.searchByName($scope.searchValue) :
+         SearchService.getByArea($scope.selectedArea);
       searchPromise
         .then(function (response) {
           if (response.data.meals) {
-            console.log("inside if")
             $scope.foods = response.data.meals;
           } else {
-            $scope.foods = "no food found";
+            $scope.errorMessage = "No food found.";
           }
         })
         .catch(function () {
-          $scope.result = "error";
+          $scope.result = "An error accured when trying to get data.";
         });
     };
+
+    $scope.init();
   },
 ]);
--- components/search/search.service.js
+++ components/search/search.service.js
@@ -1,22 +1,20 @@
-// angular.module('savorIt')
-//     .service('SearchService', function(){
-//         this.query = '';
-
-//         this.setQuery = function(query){
-//             this.query = query
-//             console.log(`${query} from setQuery in the Service!`);
-//         }
-
-//         this.getQuery = function(){
-//             return this.query
-//         }
-//     })
 angular.module("search").service("SearchService", [
   "$http",
   function ($http) {
     const baseUrl = 'https://www.themealdb.com/api/json/v1/1';
+
     this.searchByName = function (name) {
+      console.log("get by name called")
      return $http.get(`${baseUrl}/search.php?s=${name}`);
+    };
+
+    this.getAreas = function () {
+      return $http.get(`${baseUrl}/list.php?a=list`);
+    };
+
+    this.getByArea = function (area) {
+      console.log("get by area called")
+      return $http.get(`${baseUrl}/filter.php?a=${area}`);
     };
   }
 ]);
