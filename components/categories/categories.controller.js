ca2b21cfc1aafdfd568739bab3cdde5640443568
NickRoccodev11 <nchls.rcc@gmail.com>
2025-01-16 16:12:59 -0500

    display categories for browsing along with image and description of each category Driver: Nicholas, Observer: Abdelouahed

--- components/categories/categories.controller.js
+++ components/categories/categories.controller.js
@@ -1,11 +1,21 @@
-angular.module('categories', [])
-.controller('CategoriesController', [
-  '$scope',
-  function($scope){
+angular.module("categories", []).controller("CategoriesController", [
+  "$scope",
+  "CategoryService",
+  function ($scope, CategoryService) {
     $scope.catMessage = "hello frome categories!!!!!!!!";
-  }
-])
-
-
-
-
+    $scope.errorMessage = "";
+    $scope.init = function () {
+      $scope.errorMessage = "";
+      const categoryPromise = CategoryService.getCategories();
+      categoryPromise
+        .then(function (response) {
+          console.log(response);
+          $scope.categories = response.data.categories;
+        })
+        .catch(function () {
+          $scope.errorMessage = "An error accured when trying to get data.";
+        });
+    };
+    $scope.init();
+  },
+]);
--- services/category.service.js
+++ services/category.service.js
@@ -1,10 +1,10 @@
-angular.module("service").service("CategeoryService", [
+angular.module("service").service("CategoryService", [
     "$http",
     function ($http) {
       const baseUrl = 'https://www.themealdb.com/api/json/v1/1';
   
       this.getCategories = function () {
-        return $http.get(`${baseUrl}/list.php?c=list`);
+        return $http.get(`${baseUrl}/categories.php`);
       };
     }
-  ]);
+  ]); 
