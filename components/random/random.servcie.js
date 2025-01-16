716ade7347e58ac1139b4e6a7cf5be2d614d46d3
Abdelouahed Redjel <abdelouahedredjel@gmail.com>
2025-01-16 13:51:15 -0600

    Refactor the code, separate concerns by making the services by topic instead of services by page

--- components/random/random.servcie.js
+++
@@ -1,8 +0,0 @@
-angular.module("random").service("RandomService", [
-  "$http",
-  function ($http) {
-    this.searchRandom = function () {
-      return $http.get(` https://www.themealdb.com/api/json/v1/1/random.php`);
-    };
-  },
-]);
