755a73cbe72e61085531933fe63dc494a74a77ab
NickRoccodev11 <nchls.rcc@gmail.com>
2025-01-16 16:50:10 -0500

    Add fetch for recipes by category TODO: hide categories once a category is selected, and finsih 'view details' feature Driver: Nicholas Observer: Abdelouahed

--- components/categories/categories.controller.js
+++ components/categories/categories.controller.js
@@ -1,19 +1,35 @@
 angular.module("categories", []).controller("CategoriesController", [
   "$scope",
   "CategoryService",
-  function ($scope, CategoryService) {
+  "RecipeService",
+  function ($scope, CategoryService, RecipeService) {
     $scope.catMessage = "hello frome categories!!!!!!!!";
     $scope.errorMessage = "";
+    $scope.selectedCategory = "";
+    $scope.recipes = [];
+    $scope.categories = [];
     $scope.init = function () {
       $scope.errorMessage = "";
       const categoryPromise = CategoryService.getCategories();
       categoryPromise
         .then(function (response) {
-          console.log(response);
           $scope.categories = response.data.categories;
         })
         .catch(function () {
           $scope.errorMessage = "An error accured when trying to get data.";
+        });
+    };
+    $scope.viewMeals = function (strCategory) {
+      $scope.recipes = [];
+      $scope.selectedCategory = strCategory;
+      const recipesPromise = RecipeService.getByCategory(strCategory);
+      recipesPromise
+        .then(function (response) {
+          $scope.recipes = response.data.meals;
+          console.log("scope.recip.", $scope.recipes);
+        })
+        .catch(function () {
+          $scope.errorMessage = "Error fetching the recipes for that category";
         });
     };
     $scope.init();
